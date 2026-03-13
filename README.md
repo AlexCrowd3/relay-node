9175ea03ca910b2b6995a196e5ef84c85d2a12fc
Alex PC <sasatkachev06@gmail.com>
2026-03-13 16:13:13 +0300

    add deploy script

---
+++ README.md
@@ -0,0 +1,5 @@
+## Command for start 
+
+``
+bash <(curl -s https://raw.githubusercontent.com/AlexCrowd3/relay-node/main/deploy.sh)
+``
--- server/message.go
+++ server/message.go
@@ -1,8 +1,8 @@
 package server
 
 type Message struct {
-	Type string `json:"type"`
-	From string `json:"from"`
-	To   string `json:"to"`
-	Data string `json:"data"`
+	Type string                 `json:"type"`
+	From string                 `json:"from"`
+	To   string                 `json:"to"`
+	Data map[string]interface{} `json:"data"`
 }
